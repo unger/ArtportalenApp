451dd99ddcf972213907ce92a5c5fb30919ba886
Magnus Unger <magnus.unger@layer10.se>
2015-12-13 00:25:53 +0100

    Switched to appcompat theme

--- ArtportalenApp.Droid/MainActivity.cs
+++ ArtportalenApp.Droid/MainActivity.cs
@@ -13,8 +13,8 @@ using Xamarin.Forms.Platform.Android;
 
 namespace ArtportalenApp.Droid
 {
-    [Activity(Label = "Artportalen", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : FormsApplicationActivity
+    [Activity(Label = "Artportalen", Icon = "@drawable/icon", Theme = "@style/Theme.AppCompat.NoActionBar", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
